6ba1c4e91181d346afcb48652e9ce061d267de6d
Raveendra Tudangil <raveendra.tudangil@gmail.com>
2020-03-12 23:27:00 +0530

    001 - Rain Water Trapped

---
+++ Scaler Academy/Accelerate Your Tech Career/Day 2/Assignment Problems/001-Rain Water Trapped/src/io/github/raveerocks/Test.java
@@ -0,0 +1,8 @@
+package io.github.raveerocks;
+public class Test {
+    public static void main(String[] args) {
+       int A [] ={0,1,0,2,1,0,1,3,2,1,2,1};
+       Solution solution = new Solution();
+        System.out.println("Total Water accumulated :"+solution.trap(A));
+    }
+}
